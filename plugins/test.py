9e3bdd99fcf5ba1d6a3df6014e7899e191993a7c
YTY <2954452539@qq.com>
2026-06-18 19:34:13 +0800

    Restrict test command to superusers

--- plugins/test.py
+++ plugins/test.py
@@ -1,7 +1,8 @@
 from nonebot import on_command
 from nonebot.adapters.onebot.v11 import MessageEvent
+from nonebot.permission import SUPERUSER
 
-test = on_command("test", aliases={"测试", "ping"}, priority=5)
+test = on_command("test", aliases={"测试", "ping"}, permission=SUPERUSER, priority=5)
 
 
 @test.handle()
